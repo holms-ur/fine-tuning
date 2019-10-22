7f682b7de7aa724c66aa5fb2653be39a255c1bd9
Jónathan Heras <joheras@gmail.com>
2019-10-22 09:46:46 +0200

    Add files via upload

---
+++ code/yolo/yolo-opencv-tablas.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Download YOLO necessary files.
+
+# Initialize parameters.
+
+# In[1]:
+
+
+confThreshold = 0.25  #Confidence threshold
+nmsThreshold = 0.45   #Non-maximum suppression threshold
+
+inpWidth = 416      #Width of network's input image
+inpHeight = 416       #Height of network's input image
+
+
+# Import libraries and define auxiliary functions.
+
+# In[2]:
+
+
+import cv2 as cv
+import numpy as np
+
+
+# In[3]:
+
+
+# Get the names of the output layers
+def getOutputsNames(net):
+    # Get the names of all the layers in the network
+    layersNames = net.getLayerNames()
+    for i in net.getUnconnectedOutLayers():
+        print(i[0]-1)
+    # Get the names of the output layers, i.e. the layers with unconnected outputs
+    return [layersNames[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+# Draw the predicted bounding box
+def drawPred(frame,classId, conf, left, top, right, bottom):
+    # Draw a bounding box.
+    cv.rectangle(frame, (left, top), (right, bottom), (255, 178, 50), 3)
+    
+    label = '%.2f' % conf
+        
+    # Get the label for the class name and its confidence
+    if classes:
+        assert(classId < len(classes))
+        label = '%s:%s' % (classes[classId], label)
+
+    #Display the label at the top of the bounding box
+    labelSize, baseLine = cv.getTextSize(label, cv.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+    top = max(top, labelSize[1])
+    #cv.rectangle(frame, (left, top - round(1.5*labelSize[1])), (left + round(1.5*labelSize[0]), top + baseLine), (255, 255, 255), cv.FILLED)
+    cv.putText(frame, label, (left, top), cv.FONT_HERSHEY_SIMPLEX, 0.75, (0,0,0), 1)
+
+# Remove the bounding boxes with low confidence using non-maxima suppression
+def postprocess(frame, outs):
+    frameHeight = frame.shape[0]
+    frameWidth = frame.shape[1]
+
+    classIds = []
+    confidences = []
+    boxes = []
+    # Scan through all the bounding boxes output from the network and keep only the
+    # ones with high confidence scores. Assign the box's class label as the class with the highest score.
+    classIds = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            classId = np.argmax(scores)
+            confidence = scores[classId]
+            if confidence > confThreshold:
+                center_x = int(detection[0] * frameWidth)
+                center_y = int(detection[1] * frameHeight)
+                width = int(detection[2] * frameWidth)
+                height = int(detection[3] * frameHeight)
+                left = int(center_x - width / 2)
+                top = int(center_y - height / 2)
+                classIds.append(classId)
+                confidences.append(float(confidence))
+                boxes.append([left, top, width, height])
+
+    # Perform non maximum suppression to eliminate redundant overlapping boxes with
+    # lower confidences.
+    indices = cv.dnn.NMSBoxes(boxes, confidences, confThreshold, nmsThreshold)
+    for i in indices:
+        i = i[0]
+        box = boxes[i]
+        left = box[0]
+        top = box[1]
+        width = box[2]
+        height = box[3]
+        drawPred(frame,classIds[i], confidences[i], left, top, left + width, top + height)
+    return boxes
+
+
+# Load the model and classes. 
+
+# In[4]:
+
+
+# Load names of classes
+classesFile = "vocTablas.names";
+classes = None
+with open(classesFile, 'rt') as f:
+    classes = f.read().rstrip('\n').split('\n')
+ 
+# Give the configuration and weight files for the model and 
+# load the network using them.
+modelConfiguration = "tablasFinaltest416320.cfg";
+modelWeights = "tablasFinaltrain_10000.weights"
+
+net = cv.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
+
+
+# Read the input for the case of an image. 
+
+# In[5]:
+
+
+
+
+
+# The input image to a neural network needs to be in a certain format called a blob.
+# 
+# After a frame is read from the input image or video stream, it is passed through the blobFromImage function to convert it to an input blob for the neural network. In this process, it scales the image pixel values to a target range of 0 to 1 using a scale factor of 1/255. It also resizes the image to the given size of (416, 416) without cropping. Note that we do not perform any mean subtraction here, hence pass [0,0,0] to the mean parameter of the function and keep the swapRB parameter to its default value of 1.
+# 
+# The output blob is then passed in to the network as its input and a forward pass is run to get a list of predicted bounding boxes as the network’s output. These boxes go through a post-processing step in order to filter out the ones with low confidence scores. We will go through the post-processing step in more detail in the next section. We print out the inference time for each frame at the top left. The image with the final bounding boxes is then saved to the disk, either as an image for an image input or using a video writer for the input video stream.
+
+# In[6]:
+
+
+import imutils
+
+
+# In[7]:
+
+
+
+
+
+# In[8]:
+
+
+# Create a 4D blob from a frame.
+#frame = cv.resize(frame,(inpWidth, inpHeight))
+def predict(name):
+    frame = cv.imread(name)
+    blob = cv.dnn.blobFromImage(frame, 1/255, (inpWidth, inpHeight), [0,0,0], 1, crop=False)
+
+    # Sets the input to the network
+    net.setInput(blob)
+
+    # Runs the forward pass to get output of the output layers
+    outs = net.forward(getOutputsNames(net))
+
+    # Remove the bounding boxes with low confidence
+    boxes1=postprocess(frame, outs)
+    print(len(boxes1))
+    # Put efficiency information. The function getPerfProfile returns the 
+    # overall time for inference(t) and the timings for each of the layers(in layersTimes)
+    t, _ = net.getPerfProfile()
+    #label = 'Inference time: %.2f ms' % (t * 1000.0 / cv.getTickFrequency())
+    #cv.putText(frame, label, (0, 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
+    cv.imwrite("prediction.jpg",frame) 
+
+
+# In[ ]:
+
+
+
+
